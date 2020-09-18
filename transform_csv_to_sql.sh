2ca9a97cb35e1ef39a5e9579f2cdd2e71d5c41b4
Xueying Ding <xueying.ding@vanderbilt.edu>
2020-09-18 23:43:00 +0800

    Add files via upload

--- transform_csv_to_sql.sh
+++ transform_csv_to_sql.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 head -n 1000 result.csv | tr [:upper:] [:lower:] | tr ' ' '_' | sed 's/#/num/' | csvsql -i postgresql --db-schema xueying_ding_acs --tables acs_result_table | tee create_table.sql
